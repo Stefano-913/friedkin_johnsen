5beb989abc488b876037871007ab004037dda9a5
Stefano-913 <stefanoliazza@gmail.com>
2026-08-27 23:15:10 +0200

    feat: matrix_conversion module for conversion of input to numpy arrays.
    Functions added are:
    - to_array(): convert the input to floating-point numpy array.
    - to_2D_matrix(): convert the input to floating-point numpy array and
    check that it's a 2D structure specifically.
    Tests are included.

---
+++ matrix_conversion.py
@@ -0,0 +1,134 @@
+import numpy as np
+import numpy.typing as npt
+import ast
+
+from .error_messages import item_summary_for_error
+
+
+def to_array(array: str | npt.ArrayLike,
+             array_error_name: str = "") -> np.ndarray:
+    """ Convert the input to a floating-point Numpy ndarray.
+
+        Strings are also accepted, provided they are convertible
+        to an array-like object and do not need to be evaluated
+        (e.g. "2+5", "np.sin(1)" or "np.e" are not be considered valid inputs).
+
+        Does not accept objects containing complex numbers, infinite values or
+        NaNs.
+
+        Parameters
+        ----------
+        array : str or array_like
+            Array-like object (or string representing one) to be converted to
+            Numpy ndarray.
+
+        array_error_name : str, optional
+            Name of the array to be used in error messages, by default an empty
+             string.
+
+        Returns
+        -------
+        array: np.ndarray
+            A floating-point Numpy ndarray.
+
+        Raises
+        ------
+        TypeError
+            If the input cannot be converted to a floating-point ndarray
+            or contains complex numbers.
+
+        ValueError
+            If the input object contains NaN, infinite values or if the string
+            contains an invalid expression.
+
+        SyntaxError
+            If the input is entered by a string with a syntax error.
+    """
+    array_summary = item_summary_for_error(array, array_error_name)
+
+    if isinstance(array, str):
+        if array == "":
+            return np.array([], dtype=float)
+
+        try:
+            array = ast.literal_eval(array)
+        except (ValueError) as e:
+            raise ValueError(f"String '{array_summary}' contains invalid "
+                             f"expression that might need evaluation.\n"
+                             f"{e}") from e
+
+        except (SyntaxError) as e:
+            raise SyntaxError(f"String '{array_summary}' contains a syntax "
+                              f"error that invalidates the input.\n{e}") from e
+
+    try:
+        array = np.asarray(array)
+    except (ValueError) as e:
+        raise ValueError(f"{array_summary} of {type(array)} type cannot be "
+                         f"converted to a matrix.\n{e}") from e
+
+    if np.iscomplexobj(array):
+        raise TypeError(f"{array_summary} contains complex values, "
+                        f"which are not supported.")
+
+    try:
+        array = array.astype(float)
+    except (TypeError) as e:
+        raise TypeError(f"Values in {array_summary} of {type(array)} type "
+                        f"cannot be converted to floating-point.\n{e}") from e
+
+    if not np.isfinite(array).all():
+        raise ValueError(f"{array_summary} contains NaN or infinite values.")
+
+    return array
+
+
+def to_2D_matrix(mtx: str | npt.ArrayLike,
+                 array_error_name: str = "") -> np.ndarray:
+    """ Converts the input to a floating point 2D matrix.
+        Strings are also accepted, provided they are convertible to an
+        array-like object.
+        One dimensional matrices and scalars will also be accepted as
+        degenerate cases and interpreted as column vectors.
+        Objects that result in a dimensionality higher than 2 will not be
+        accepted.
+
+        Does not accept objects containing complex numbers, infinite
+        values or NaNs.
+
+        Parameters
+        ----------
+        mtx : str or array_like
+            Array-like object (or string representing one) to be interpreted
+            as a matrix. Must have a dimensionality of either one or two.
+
+        Returns
+        -------
+        np.ndarray
+            Returns a floating-point Numpy ndarray.
+
+        Raises
+        ------
+        TypeError
+            If the input cannot be converted to a floating-point ndarray
+            or contains complex numbers.
+        ValueError
+            If the input object has dimensionality higher than 2 or
+            if contains NaN or infinite values.
+
+        See Also
+        --------
+        to_array : Convert input to a floating-point ndarray.
+    """
+
+    mtx = to_array(mtx, array_error_name=array_error_name)
+
+    if mtx.ndim == 1:
+        mtx = mtx.reshape(-1, 1)
+
+    elif mtx.ndim != 2:
+        raise ValueError(f"In {item_summary_for_error(mtx, array_error_name)} "
+                         f"expected a matrix of dimension either one or two, "
+                         f"got ndim={mtx.ndim}.")
+
+    return mtx
---
+++ tests/test_matrix_conversion.py
@@ -0,0 +1,188 @@
+import pytest
+import sys
+import numpy as np
+from ..matrix_conversion import to_array
+from ..matrix_conversion import to_2D_matrix
+
+# --------
+
+
+class TestToArray:
+
+    @pytest.mark.parametrize("input_array_like, expected_array", [
+         ([], np.array([], dtype=float)),
+         ((), np.array([], dtype=float)),
+         (np.array([]), np.array([], dtype=float)),
+         ("", np.array([], dtype=float)),
+
+         (1, np.array(1, dtype=float)),
+         ([1], np.array([1], dtype=float)),
+         ("-1", np.array(-1, dtype=float)),
+         ((np.pi), np.array(np.pi, dtype=float)),
+         (True, np.array(1, dtype=float)),
+        ])
+    def test_0_dimensional(self, input_array_like, expected_array):
+        assert np.array_equal(to_array(input_array_like), expected_array)
+
+    @pytest.mark.parametrize("input_array_like, expected_array", [
+         ([1, 2, 3], np.array([1, 2, 3], dtype=float)),
+         ((0, 0, 0), np.array([0, 0, 0], dtype=float)),
+         (["1", "2", "3"], np.array([1, 2, 3], dtype=float)),
+         ("(1, 2, 3)", np.array([1, 2, 3], dtype=float)),
+
+         (np.array([-4, +2, 2+3], dtype=float),
+          np.array([-4, 2, 5], dtype=float))
+        ])
+    def test_1_dimensional(self, input_array_like, expected_array):
+        assert np.array_equal(to_array(input_array_like), expected_array)
+
+    @pytest.mark.parametrize("input_array_like, expected_array", [
+         ([[1], [2]], np.array([[1], [2]], dtype=float)),
+         ([[1, 2], [3, 4]], np.array([[1, 2], [3, 4]], dtype=float)),
+         ([[1, 2], [3, 4], [5, 6]],
+          np.array([[1, 2], [3, 4], [5, 6]], dtype=float)),
+
+         ([[[1, 2], [3, 4]], [[5, 6], [7, 8]]],
+          np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]], dtype=float)),
+
+         (np.array([[1, 2], [3, 4]]), np.array([[1, 2], [3, 4]], dtype=float)),
+         ("[[1, 2],[3, 4]]", np.array([[1, 2], [3, 4]], dtype=float))
+        ])
+    def test_multi_dimensional(self, input_array_like, expected_array):
+        assert np.array_equal(to_array(input_array_like), expected_array)
+
+    @pytest.mark.parametrize("input_array_like, expected_array", [
+         ([[]], np.array([[]], dtype=float)),
+         ([()], np.array([[]], dtype=float)),
+
+         ([[], [], []], np.array([[], [], []], dtype=float)),
+
+         ([[[], []], [[], []]], np.array([[[], []], [[], []]], dtype=float)),
+        ])
+    def test_empty_structures(self, input_array_like, expected_array):
+        assert np.array_equal(to_array(input_array_like), expected_array)
+
+    @pytest.mark.parametrize("input_array_like, expected_array", [
+         ([[1, 2], [3, 4]], np.array([[1, 2], [3, 4]], dtype=float)),
+         (([1, 2], [3, 4]), np.array([[1, 2], [3, 4]], dtype=float)),
+         ([(1, 2), (3, 4)], np.array([[1, 2], [3, 4]], dtype=float)),
+         (((1, 2), (3, 4)), np.array([[1, 2], [3, 4]], dtype=float))
+        ])
+    def test_various_input_formats(self, input_array_like, expected_array):
+        assert np.array_equal(to_array(input_array_like), expected_array)
+
+    @pytest.mark.parametrize("string_input", [
+         "invalid_string",
+         "2+5",
+         "np.pi",
+         "np.inf",
+         "[1,2,3-4]",
+         "np.sin(1)",
+         "[np.sin(1), np.cos(1)]",
+         ])
+    def test_error_string_to_interpret(self, string_input):
+        with pytest.raises(ValueError,
+                           match=r"String '.*' contains invalid "
+                           "expression that might need evaluation.\n.*"):
+            to_array(string_input)
+
+    @pytest.mark.parametrize("string_input", [
+         "0)",
+         "[2, 3}",
+         "[1, 2, 3",
+         "(1",
+         ])
+    def test_error_syntax_string_(self, string_input):
+        with pytest.raises(SyntaxError,
+                           match=r"String '.*' contains a syntax error "
+                           "that invalidates the input.\n.*"):
+            to_array(string_input)
+
+    @pytest.mark.parametrize("not_array_like_input", [
+        [1, 2, [3, 4]],
+        [1, 2, [[], []]],
+        [1, 2, (3, 4)]
+        ])
+    def test_error_not_convertible(self, not_array_like_input):
+        with pytest.raises(ValueError, match=r".* of <class '.*'> type cannot "
+                           r"be converted to a matrix.\n.*"):
+            to_array(not_array_like_input)
+
+    @pytest.mark.parametrize("not_floating_point_input", [
+         object(),
+         {1, 2, 3},
+         {1: "a", 2: "b"}
+         ])
+    def test_error_not_convertible_to_float(self, not_floating_point_input):
+        with pytest.raises(TypeError, match=r".* of <class '.*'> type cannot "
+                           r"be converted to floating-point.\n.*"):
+            to_array(not_floating_point_input)
+
+    def test_error_complex_numbers(self):
+        with pytest.raises(TypeError):
+            to_array([1, 2, 3+4j])
+        with pytest.raises(TypeError):
+            to_array("[1, 2, 3+4j]")
+        with pytest.raises(TypeError):
+            to_array(np.array([1, 2, 3], dtype=complex))
+
+    def test_error_nan_or_inf(self):
+        with pytest.raises(ValueError,
+                           match=r".*contains NaN or infinite values."):
+            to_array([np.nan])
+
+        with pytest.raises(ValueError,
+                           match=r".*contains NaN or infinite values."):
+            to_array([1, 2, 3, np.nan])
+
+        with pytest.raises(ValueError,
+                           match=r".*contains NaN or infinite values."):
+            to_array([np.inf])
+
+        with pytest.raises(ValueError,
+                           match=r".*contains NaN or infinite values."):
+            # None converts to 'nan' during astype(float)
+            to_array(None)
+
+# --------
+
+
+class TestTo2DMatrix:
+
+    @pytest.mark.parametrize("input_array_like", [
+        (np.array([])),
+        (np.array([1])),
+        (np.array([1, 2, 3]))
+    ])
+    def test_if_vector(self, input_array_like):
+        array = to_2D_matrix(input_array_like)
+        assert array.ndim == 2
+        assert array.shape[1] == 1
+
+    @pytest.mark.parametrize("input_array_like, expected_shape", [
+        (np.array([[]]), (1, 0)),
+        (np.array([[], [], []]), (3, 0)),
+        (np.array([[1], [2], [3]]), (3, 1)),
+        (np.array([[1, 2, 3]]), (1, 3)),
+        (np.array([[1, 2, 3], [4, 5, 6]]), (2, 3))
+    ])
+    def test_if_2D_matrix(self, input_array_like, expected_shape):
+        array = to_2D_matrix(input_array_like)
+        assert array.ndim == 2
+        assert array.shape == expected_shape
+
+    @pytest.mark.parametrize("input_array_like", [
+        (np.array([[[]]])),
+        (np.array([[[1, 2], [3, 4]]])),
+        (np.array([[[0, 0], [0, 0]], [[1, 2], [3, 4]]]))
+    ])
+    def test_error_if_not_2_dimensional(self, input_array_like):
+        with pytest.raises(ValueError,
+                           match=r".* expected a matrix of dimension "
+                           "either one or two.*"):
+            to_2D_matrix(input_array_like)
+
+
+if __name__ == "__main__":
+    sys.exit(pytest.main(
+        ["-v", "--cov=your_package", "--cov-report=term-missing"]))
